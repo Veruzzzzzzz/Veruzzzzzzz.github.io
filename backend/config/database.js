06725bc4466fb1fd3e8ff1d446fc3a5d10a2a5d7
Veruzzzzzzz <veruzotavio58@gmail.com>
2025-01-06 00:21:34 -0300

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,12 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('loja', 'root', 'root', {
-    host: 'dpg-cttk7otds78s73cok1qg-a',
+const sequelize = new Sequelize({
+    host: 'localhost',
     dialect: 'postgres',
-    user :veruzdatabaseloja_user,
+    user: 'veruzbancodedados_user',
     port: 5432,
-    password: Aui9mfKPsMjkYDE5jhN1RKsA87lHxu4b,
-    database: veruzdatabaseloja
+    password: 'sXXUxmXNvJQszKYreukIUlInIX5pk8WZ',
+    database: 'veruzbancodedados'
 });
 
-module.exports = sequelize; 
+module.exports = sequelize;
